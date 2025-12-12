011abbdb3d46743551e8809d3dbf3e98a2bc29bd
朝倉水希 <asakuramizu111@gmail.com>
2025-12-12 14:03:00 +0800

    feat: iofn

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -2,8 +2,9 @@ mod chain;
 mod copy;
 mod cursor;
 mod empty;
+mod iofn;
 mod repeat;
 mod sink;
 mod take;
 
-pub use self::{chain::*, copy::*, cursor::*, empty::*, repeat::*, sink::*, take::*};
+pub use self::{chain::*, copy::*, cursor::*, empty::*, iofn::*, repeat::*, sink::*, take::*};
