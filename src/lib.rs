b5879d4892449c3ebc32f3c29e021913f1a7c91b
朝倉水希 <asakuramizu111@gmail.com>
2025-12-24 17:18:23 +0800

    feat: add utils

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,8 @@
 #![cfg_attr(not(doc), no_std)]
 #![feature(doc_cfg)]
 #![feature(core_io_borrowed_buf)]
-#![cfg_attr(not(borrowedbuf_init), feature(maybe_uninit_fill))]
+#![feature(min_specialization)]
+#![feature(maybe_uninit_fill)]
 #![cfg_attr(not(maybe_uninit_slice), feature(maybe_uninit_slice))]
 #![warn(missing_docs)]
 
@@ -19,9 +20,10 @@ mod buffered;
 pub mod prelude;
 mod read;
 mod seek;
+mod utils;
 mod write;
 
-pub use self::{buffered::*, read::*, seek::*, write::*};
+pub use self::{buffered::*, read::*, seek::*, utils::*, write::*};
 
 /// I/O poll results.
 #[derive(Debug, Default, Clone, Copy)]
