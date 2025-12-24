c929ae8ae3fb3ea6cc0310f9c800517d18d3999a
朝倉水希 <asakuramizu111@gmail.com>
2025-12-24 16:41:09 +0800

    feat: probe maybe_uninit_slice

--- build.rs
+++ build.rs
@@ -1,8 +1,9 @@
 fn main() {
-    autocfg::emit_possibility("borrowedbuf_init");
     autocfg::rerun_path("build.rs");
 
     let ac = autocfg::new();
+
+    autocfg::emit_possibility("borrowedbuf_init");
     let code = r#"
         #![no_std]
         #![feature(core_io_borrowed_buf)]
@@ -12,5 +13,16 @@ fn main() {
     "#;
     if ac.probe_raw(code).is_ok() {
         autocfg::emit("borrowedbuf_init");
+    }
+
+    autocfg::emit_possibility("maybe_uninit_slice");
+    let code = r#"
+        #![no_std]
+        pub fn probe() {
+            let _ = <[core::mem::MaybeUninit<()>]>::assume_init_mut;
+        }
+    "#;
+    if ac.probe_raw(code).is_ok() {
+        autocfg::emit("maybe_uninit_slice");
     }
 }
